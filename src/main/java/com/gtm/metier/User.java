eb2c585d121f00902d25663fbf7b2ad088c18545
izardj <izard.jerome@free.fr>
2017-05-16 14:35:34 +0200

    Configuration d'hibernate en tant que bean Spring

--- src/main/java/com/gtm/metier/User.java
+++ src/main/java/com/gtm/metier/User.java
@@ -1,12 +1,20 @@
 package com.gtm.metier;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
 @Scope("prototype")
+@Entity
 public class User {
 
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
 	private long id;
 	private String nom;
 	private String prenom;
